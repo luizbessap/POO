1eedebe64345d3bdc4707d63048194e4794b5055
Luiz Felipe Bessa de Paula <52763266+luizbessap@users.noreply.github.com>
2023-09-21 09:42:44 -0300

    Add files via upload

---
+++ Atividade 2/Questão 1/Cachorro.java
@@ -0,0 +1,21 @@
+public class Cachorro extends Animal {
+    private String raca;
+
+    public Cachorro(String nome, double peso, String raca) {
+        super(nome, peso);
+        this.raca = raca;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Raça: " + raca;
+    }
+}
